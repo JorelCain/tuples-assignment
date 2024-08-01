5f2167dbccb1f87da3cc10ce39369b19eba7f07e
JorelCain <jorelcain2@gmail.com>
2024-08-01 13:58:49 -0400

    Initial Commit

--- tuple_mastery.py
+++ tuple_mastery.py
@@ -1,7 +1,7 @@
 # Tuple Mastery
 
 # Formatting Flight Itineraries: 
-# 
+
 # Create a Python function that takes a list of tuples as an argument. 
 # Each tuple contains information about a flight itinerary: (traveler_name, origin, destination). 
 # The function should loop through the list of itineraries and print a formatted string for each. 
