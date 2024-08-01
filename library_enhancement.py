563895f4bdb4cb1cdbf595ee43c9b67ca8a22141
JorelCain <jorelcain2@gmail.com>
2024-08-01 13:57:50 -0400

    Initial Commit

--- library_enhancement.py
+++ library_enhancement.py
@@ -1,6 +1,9 @@
 # Python Data Structure Challenge
 
-# Library System Enhancement: You are maintaining a library system where each book is stored as a tuple within a list. Your task is to update this system by adding new books and ensuring no duplicates.
+# Library System Enhancement: 
+
+# You are maintaining a library system where each book is stored as a tuple # within a list.
+# Your task is to update this system by adding new books and ensuring no duplicates.
 
 library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")]
 
--- tuple_mastery.py
+++ tuple_mastery.py
@@ -1,6 +1,16 @@
 # Tuple Mastery
 
-# Formatting Flight Itineraries: Create a Python function that takes a list of tuples as an argument. Each tuple contains information about a flight itinerary: (traveler_name, origin, destination). The function should loop through the list of itineraries and print a formatted string for each. For example, if the input is [("Alice", "New York", "London"), ("Bob", "Tokyo", "San Francisco")], the output should be:
+# Formatting Flight Itineraries: 
+# 
+# Create a Python function that takes a list of tuples as an argument. 
+# Each tuple contains information about a flight itinerary: (traveler_name, origin, destination). 
+# The function should loop through the list of itineraries and print a formatted string for each. 
+
+# For example, if the input is 
+# [("Alice", "New York", "London"), ("Bob", "Tokyo", "San Francisco")], 
+
+# the output should be: 
+# "Itinerary 1: Alice - From New York to London" "Itinerary 2: Bob - From Tokyo to San Francisco"
 
 itineraries = [("Alice", "New York", "London"), ("Bob", "Tokyo", "San Francisco")]
 
